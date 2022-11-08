0a58d9886d276cee069f2e4833f725ae08cb1668
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-11-08 18:15:20 -0300

    requisição a api viacep finalizada

--- cadastro/viacep.js
+++ cadastro/viacep.js
@@ -1,17 +1,28 @@
 const inputCEP = document.querySelector('#floatingCep');
+const inputEstado = document.querySelector('#floatingEstado');
+const inputCidade = document.querySelector('#floatingCidade');
+const inputBairro = document.querySelector('#floatingBairro');
+const inputRua = document.querySelector('#floatingRua');
+const inputComplemento = document.querySelector('#floatingComplemento');
 let dados;
 
-async function getInfo({ target }) {
-  const cep = target.value;
+// FUNÇÃO PARA FAZER A REQUISIÇÃO DOS DADOS ATRAVÉS DA API VIACEP
+// E GUARDAR OS VALORES NA VARIÁVEL DADOS
+async function getInfo(event) {
+  const cep = event.target.value;
   const url = `https://viacep.com.br/ws/${cep}/json/`;
   const response = await fetch(url);
   dados = await response.json();
-  console.log(dados);
 }
 
-function setInfo() {
-  getInfo();
-  // criar
+// FUNÇÃO PARA SETAR OS CAMPOS BUSCADOS PELA API E AUTOCOMPLETAR PARA O USUÁRIO
+async function setInfo() {
+  await getInfo(event);
+  inputEstado.value = dados.uf;
+  inputCidade.value = dados.localidade;
+  inputBairro.value = dados.bairro;
+  inputRua.value = dados.logradouro;
+  inputComplemento.value = dados.complemento;
 }
 
-inputCEP.addEventListener('change', getInfo);
+inputCEP.addEventListener('change', setInfo);
