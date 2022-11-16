aacd799f094577cb0abc26d6e13791002931082c
fernandorussie <fernandocesarrussiefelip@gmail.com>
2022-11-16 16:49:48 -0300

    criando localStorage

--- produtos/script.js
+++ produtos/script.js
@@ -18,28 +18,87 @@ $('#exampleModal').on('show.bs.modal', function (event) {
   })
 
 
+  // FUNÇÃO PARA LocalStorage
+  localStorage.setItem('btn','true');
+
   // Função Dark/Ligth Mode
+
   function darkMode(){
     var checkBox = document.querySelector('#checkbox');
-    var video = document.querySelector('#video');
-    var image = document.querySelector('#image');
+    var checkBoxV = document.querySelector('#checkbox').value;
+    var video = document.querySelector('.video');
+    var nav = document.querySelector('nav');
+    var footer = document.querySelector('footer');
+    
+    localStorage.setItem('checkBoxValue', checkBoxV)
+
     if (checkBox.checked == true){
-        document.documentElement.style.setProperty('--bg-cinza', 'rgba(152,24,245, 0.9)'),
-        document.documentElement.style.setProperty('--cinza', '#9818F5'),
+        footer.style.setProperty('--branco','#fff'),
+        nav.style.setProperty('--branco','#fff'),
+        document.documentElement.style.setProperty('--bg-cinza', 'rgba(151, 151, 149, 0.9)'),
+        document.documentElement.style.setProperty('--cinza', '#A9A9A9'),
         document.documentElement.style.setProperty('--cinza-escuro', '#C7C8CA'),
         document.documentElement.style.setProperty('--branco', '#000'),
-        document.documentElement.style.setProperty('--border-card', 'rgba(152,24,245, 0.37)'),
-        document.documentElement.style.setProperty('--verde', '#BD62FF'),
-        video.setAttribute ('src', '../assets/cellcode-Iphone/video-cell-modelo-black.mp4');
-        image.setAttribute ('src', '../assets/cellcode-Iphone/tablet.png');
+        video.setAttribute ('src', '../assets/cellcode-Iphone/video-2cell-gray.mp4');
       } else {
         document.documentElement.style.setProperty('--bg-cinza', 'rgba(57, 62, 70, 0.9)'),
         document.documentElement.style.setProperty('--cinza-escuro', '#222831'),
         document.documentElement.style.setProperty('--cinza', '#393e46'),
         document.documentElement.style.setProperty('--branco', '#fff'),
-        document.documentElement.style.setProperty('--border-card', 'rgba( 50, 221, 224, 0.37)'),
-        document.documentElement.style.setProperty('--verde', '#1af38c'),
-        video.setAttribute ('src','../assets/cellcode-Iphone/videoCellCode.mp4'),
-        image.setAttribute ('src', '../assets/cellcode-Iphone/img2.png');
+        video.setAttribute ('src','../assets/cellcode-Iphone/video-2cell-black.mp4')
       }
     }
+    document.onchange = darkMode;
+
+//Versão funcional só nos produtos
+  // function darkMode(){
+  //   var checkBox = document.querySelector('#checkbox');
+  //   var video = document.querySelector('.video');
+  //   var nav = document.querySelector('nav');
+  //   var footer = document.querySelector('footer');
+    
+  //   if (checkBox.checked == true){
+  //       footer.style.setProperty('--branco','#fff'),
+  //       nav.style.setProperty('--branco','#fff'),
+  //       document.documentElement.style.setProperty('--bg-cinza', 'rgba(151, 151, 149, 0.9)'),
+  //       document.documentElement.style.setProperty('--cinza', '#A9A9A9'),
+  //       document.documentElement.style.setProperty('--cinza-escuro', '#C7C8CA'),
+  //       document.documentElement.style.setProperty('--branco', '#000'),
+  //       video.setAttribute ('src', '../assets/cellcode-Iphone/video-2cell-gray.mp4');
+  //     } else {
+  //       document.documentElement.style.setProperty('--bg-cinza', 'rgba(57, 62, 70, 0.9)'),
+  //       document.documentElement.style.setProperty('--cinza-escuro', '#222831'),
+  //       document.documentElement.style.setProperty('--cinza', '#393e46'),
+  //       document.documentElement.style.setProperty('--branco', '#fff'),
+  //       video.setAttribute ('src','../assets/cellcode-Iphone/video-2cell-black.mp4')
+  //     }
+  //   }
+
+
+  // function darkMode(){
+  //   var checkBox = document.querySelector('#checkbox');
+  //   var video = document.querySelector('#video');
+  //   var image = document.querySelector('#image');
+  //   if (checkBox.checked == true){
+  //       document.documentElement.style.setProperty('--bg-cinza', 'rgba(152,24,245, 0.9)'),
+  //       document.documentElement.style.setProperty('--cinza', '#9818F5'),
+  //       document.documentElement.style.setProperty('--cinza-escuro', '#C7C8CA'),
+  //       document.documentElement.style.setProperty('--branco', '#000'),
+  //       document.documentElement.style.setProperty('--border-card', 'rgba(152,24,245, 0.37)'),
+  //       document.documentElement.style.setProperty('--verde', '#BD62FF'),
+  //       video.setAttribute ('src', '../assets/cellcode-Iphone/video-cell-modelo-black.mp4');
+  //       image.setAttribute ('src', '../assets/cellcode-Iphone/tablet.png');
+  //     } else {
+  //       document.documentElement.style.setProperty('--bg-cinza', 'rgba(57, 62, 70, 0.9)'),
+  //       document.documentElement.style.setProperty('--cinza-escuro', '#222831'),
+  //       document.documentElement.style.setProperty('--cinza', '#393e46'),
+  //       document.documentElement.style.setProperty('--branco', '#fff'),
+  //       document.documentElement.style.setProperty('--border-card', 'rgba( 50, 221, 224, 0.37)'),
+  //       document.documentElement.style.setProperty('--verde', '#1af38c'),
+  //       video.setAttribute ('src','../assets/cellcode-Iphone/videoCellCode.mp4'),
+  //       image.setAttribute ('src', '../assets/cellcode-Iphone/img2.png');
+  //     }
+  //   }
+
+
+  
